421707a4e069dd21abd058e6619e5850289d5b2a
Jairo Correa <jn.j41r0@gmail.com>
2024-01-10 11:38:22 -0300

    KSampler options for inpaint

--- nodes/sampling.py
+++ nodes/sampling.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections import defaultdict
 import torch
 import math
 
@@ -7,12 +8,24 @@ from ..utils import CATEGORY_SAMPLING, DIRECTIONS
 import comfy
 from comfy import model_management
 from comfy.utils import wait_cooldown
-from nodes import common_ksampler, MAX_RESOLUTION, VAEEncode, VAEDecode, EmptyLatentImage
+from nodes import common_ksampler, MAX_RESOLUTION, VAEEncode, VAEDecode, EmptyLatentImage, VAEEncodeForInpaint, SetLatentNoiseMask
 from comfy_extras.nodes_mask import composite
 from comfy.sd import VAE
 from comfy_extras.nodes_upscale_model import ImageUpscaleWithModel
 from .facerestore import FACEDETECTION_MODELS, JN_FaceRestoreWithModel
-from .image import JN_ImageCenterArea
+from .image import JN_ImageCenterArea, JN_ImageCrop, JN_ImageUncrop, get_crop_region, normalize_area
+
+def mask_to_image(mask):
+    return mask.reshape((-1, 1, mask.shape[-2], mask.shape[-1])).movedim(1, -1).expand(-1, -1, -1, 3)
+
+def image_to_mask(image, channel="red"):
+    channels = ["red", "green", "blue", "alpha"]
+    return image[:, :, :, channels.index(channel)]
+
+def grow_mask(mask, grow_mask_by=6):
+    kernel_tensor = torch.ones((1, 1, grow_mask_by, grow_mask_by))
+    padding = math.ceil((grow_mask_by - 1) / 2)
+    return torch.clamp(torch.nn.functional.conv2d(mask, kernel_tensor, padding=padding), 0, 1)
 
 def apply_seamless(tensor, direction, border_percent):
     (batch_size, channels, height, width) = tensor.shape
@@ -243,12 +256,14 @@ class JN_KSamplerTileParams:
         }
         return (params,)
 
-class JN_KSamplerResizeInputParams:
+class JN_KSamplerResizeParams:
     CATEGORY = CATEGORY_SAMPLING
     RETURN_TYPES = ("PARAMS",)
     FUNCTION = "run"
     RESIZE_METHODS = ["nearest-exact", "bilinear", "area", "bicubic", "lanczos", "bislerp"]
     CROP_METHODS = ["disabled", "center"]
+
+    TYPE = "JN_KSamplerResizeParams"
 
     @classmethod
     def INPUT_TYPES(s):
@@ -267,7 +282,7 @@ class JN_KSamplerResizeInputParams:
 
     def run(self, method="nearest-exact", crop="disabled", width=0, height=0, scale_by=0, upscale_model=None, **kwargs):
         params = {
-            "__type__": "JN_KSamplerResizeInputParams",
+            "__type__": self.TYPE,
             "method": method,
             "crop": crop,
             "width": width,
@@ -277,38 +292,24 @@ class JN_KSamplerResizeInputParams:
         }
         return (params,)
 
-class JN_KSamplerResizeOutputParams:
-    CATEGORY = CATEGORY_SAMPLING
-    RETURN_TYPES = ("PARAMS",)
-    FUNCTION = "run"
-    RESIZE_METHODS = ["nearest-exact", "bilinear", "area", "bicubic", "lanczos", "bislerp"]
-    CROP_METHODS = ["disabled", "center"]
+class JN_KSamplerResizeInputParams(JN_KSamplerResizeParams):
+    TYPE = "JN_KSamplerResizeInputParams"
+
+class JN_KSamplerResizeOutputParams(JN_KSamplerResizeParams):
+    TYPE = "JN_KSamplerResizeOutputParams"
+
+class JN_KSamplerResizeMaskAreaParams(JN_KSamplerResizeParams):
+    TYPE = "JN_KSamplerResizeMaskAreaParams"
 
     @classmethod
     def INPUT_TYPES(s):
-        return {
-            "required": {
-                "method": (s.RESIZE_METHODS,),
-                "crop": (s.CROP_METHODS,),
-                "width": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 8}),
-                "height": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 8}),
-                "scale_by": ("FLOAT", {"default": 0, "min": 0, "max": 8, "step": 0.01}),
-            },
-            "optional": {
-                "upscale_model": ("UPSCALE_MODEL",),
-            },
-    }
+        types = super().INPUT_TYPES()
+        del types["required"]["crop"]
+        return types
 
-    def run(self, method="nearest-exact", crop="disabled", width=0, height=0, scale_by=0, upscale_model=None, **kwargs):
-        params = {
-            "__type__": "JN_KSamplerResizeOutputParams",
-            "method": method,
-            "crop": crop,
-            "width": width,
-            "height": height,
-            "scale_by": scale_by,
-            "upscale_model": upscale_model,
-        }
+    def run(self, **kwargs):
+        (params,) = super().run(**kwargs)
+        params["crop"] = "center"
         return (params,)
 
 class JN_KSamplerFaceRestoreParams:
@@ -339,8 +340,8 @@ class JN_KSamplerFaceRestoreParams:
 
 class JN_KSampler:
     CATEGORY = CATEGORY_SAMPLING
-    RETURN_TYPES = ("LATENT", "IMAGE", "IMAGE", "LATENT", "IMAGE")
-    RETURN_NAMES = ("LATENT", "IMAGE", "FINAL_IMAGE", "INPUT_LATENT", "INPUT_IMAGE")
+    RETURN_TYPES = ("LATENT", "IMAGE", "IMAGE", "LATENT", "IMAGE", "MASK")
+    RETURN_NAMES = ("LATENT", "IMAGE", "FINAL_IMAGE", "INPUT_LATENT", "INPUT_IMAGE", "INPUT_MASK")
     FUNCTION = "sample"
 
     @classmethod
@@ -362,6 +363,10 @@ class JN_KSampler:
             "optional": {
                 "latent_image": ("LATENT",),
                 "image": ("IMAGE",),
+                "mask": ("MASK",),
+                "only_mask_area": ("BOOLEAN", {"default": False}),
+                "mask_border_percent": ("FLOAT", {"default": 0.125, "min": 0, "max": 0xffffffffffffffff, "step": 0.001}),
+                "grow_mask_by": ("INT", {"default": 6, "min": 0, "max": MAX_RESOLUTION, "step": 1}),
                 "width": ("INT", {"default": 512, "min": 0, "max": MAX_RESOLUTION, "step": 8}),
                 "height": ("INT", {"default": 512, "min": 0, "max": MAX_RESOLUTION, "step": 8}),
                 "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
@@ -372,24 +377,24 @@ class JN_KSampler:
     def sample(self, model, vae, seed, steps, cfg, sampler_name, scheduler, positive, negative, denoise=1,
             decode_image=True,
             resize_input=False,
-            latent_image=None, image=None,
+            latent_image=None, image=None, mask=None, only_mask_area=False, mask_border_percent=0.125, grow_mask_by=6,
             width=512, height=512, batch_size=1,
             params=[],
             **kwargs):
         params = reduce(lambda a, b: (a if isinstance(a, list) else [a]) + (b if isinstance(b, list) else [b]), params, [None])
-        params = [param for param in params if param is not None]
-        options = {param["__type__"]: param for param in params if "__type__" in param}
+        params = [param for param in params if param is not None and "__type__" in param]
 
-        options_order = []
-        options_order_ = [param["__type__"] for param in params if "__type__" in param]
-        for option_order in options_order_:
-            if option_order not in options_order:
-                options_order.append(option_order)
+        options = defaultdict(list)
 
-        advanced_params = options["JN_KSamplerAdvancedParams"] if "JN_KSamplerAdvancedParams" in options else None
-        seamless_params = options["JN_KSamplerSeamlessParams"] if "JN_KSamplerSeamlessParams" in options else None
-        tile_params = options["JN_KSamplerTileParams"] if "JN_KSamplerTileParams" in options else None
-        resize_input_params = options["JN_KSamplerResizeInputParams"] if "JN_KSamplerResizeInputParams" in options else None
+        for param in params:
+            options[param["__type__"]].append(param)
+
+        advanced_params = options["JN_KSamplerAdvancedParams"][-1] if "JN_KSamplerAdvancedParams" in options else None
+        seamless_params = options["JN_KSamplerSeamlessParams"][-1] if "JN_KSamplerSeamlessParams" in options else None
+        tile_params = options["JN_KSamplerTileParams"][-1] if "JN_KSamplerTileParams" in options else None
+        resize_input_params = options["JN_KSamplerResizeInputParams"][-1] if "JN_KSamplerResizeInputParams" in options else None
+        resize_mask_area_params = options["JN_KSamplerResizeMaskAreaParams"][-1] if "JN_KSamplerResizeMaskAreaParams" in options else None
+
         resize_output_params = options["JN_KSamplerResizeOutputParams"] if "JN_KSamplerResizeOutputParams" in options else None
         face_restore_params = options["JN_KSamplerFaceRestoreParams"] if "JN_KSamplerFaceRestoreParams" in options else None
 
@@ -403,18 +408,84 @@ class JN_KSampler:
             resize_input_params["width"] = width
             resize_input_params["height"] = height
 
+        if resize_mask_area_params and resize_mask_area_params["width"] == 0 and resize_mask_area_params["height"] == 0 and resize_mask_area_params["scale_by"] == 0:
+            resize_mask_area_params["width"] = width
+            resize_mask_area_params["height"] = height
+
         if image is not None and resize_input_params is not None:
             image = self.resize(image.clone().movedim(-1, 1), **resize_input_params).movedim(1, -1)
 
+        if mask is not None and resize_input_params is not None:
+            mask = image_to_mask(self.resize(mask_to_image(mask).clone().movedim(-1, 1), **resize_input_params).movedim(1, -1))
+
+        if mask is not None and grow_mask_by > 0:
+            mask = grow_mask(mask, grow_mask_by)
+
+        input_image = image
+        input_mask = mask
+
+        if only_mask_area and image is not None and mask is not None:
+            mask_area = get_crop_region(mask, multiple_of=8)
+
+            if resize_mask_area_params is not None:
+                old_width = mask_area["x2"] - mask_area["x1"]
+                old_height = mask_area["y2"] - mask_area["y1"]
+
+                (new_width, new_height) = self.compute_size(original_width=old_width, original_height=old_height, **resize_mask_area_params)
+
+                old_aspect = old_width / old_height
+                new_aspect = new_width / new_height
+
+                x = 0
+                y = 0
+
+                if old_aspect > new_aspect:
+                    y = round((old_height - old_height * (old_aspect / new_aspect)) / 2)
+                elif old_aspect < new_aspect:
+                    x = round((old_width - old_width * (new_aspect / old_aspect)) / 2)
+
+                # Increase the area to have the same aspect ratio
+
+                mask_area["x1"] += x
+                mask_area["y1"] += y
+                mask_area["x2"] -= x
+                mask_area["y2"] -= y
+
+            mask_pad = int(mask_border_percent * min(mask_area["x2"] - mask_area["x1"], mask_area["y2"] - mask_area["y1"]))
+            mask_area = normalize_area(mask_area, pad=mask_pad, multiple_of=8)
+
+            image_crop_result = JN_ImageCrop().run(image=image, mask=mask, area=mask_area)
+
+            image = cropped_image = image_crop_result[0]
+            mask = cropped_mask = image_crop_result[1]
+            mask_area = image_crop_result[2]
+
+            cropped_image_width = cropped_mask.shape[-1]
+            cropped_image_height = cropped_mask.shape[-2]
+
+            if resize_mask_area_params is not None:
+                image = self.resize(cropped_image.clone().movedim(-1, 1), **resize_mask_area_params).movedim(1, -1)
+                mask = image_to_mask(self.resize(mask_to_image(cropped_mask).clone().movedim(-1, 1), **resize_mask_area_params).movedim(1, -1))
+
+        mask_applied = False
+
         if latent_image is None:
             if image is not None:
-                latent_image = VAEEncode().encode(vae=vae, pixels=image.clone())[0]
+                if model.model.inpaint_model:
+                    latent_image = VAEEncodeForInpaint().encode(vae=vae, pixels=image.clone(), mask=mask.clone(), grow_mask_by=0)[0]
+                    mask_applied = True
+                else:
+                    latent_image = VAEEncode().encode(vae=vae, pixels=image.clone())[0]
+
                 resize_input_params = None
             else:
                 latent_image = EmptyLatentImage().generate(width=width, height=height, batch_size=batch_size)[0]
 
         if resize_input_params is not None:
             latent_image["samples"] = self.resize(latent_image["samples"], samples_type="latent", **resize_input_params)
+
+        if mask is not None and not mask_applied:
+            latent_image = SetLatentNoiseMask().set_mask(samples=latent_image, mask=mask.clone())[0]
 
         if advanced_params is not None:
             disable_noise = not advanced_params["add_noise"]
@@ -461,23 +532,36 @@ class JN_KSampler:
             wait_cooldown(kind="execution")
             final_image = output_image = VAEDecode().decode(vae=vae, samples=output_latent)[0]
 
-            for option_order in options_order:
-                if option_order == "JN_KSamplerResizeOutputParams" and resize_output_params is not None:
-                    wait_cooldown(kind="execution")
-                    final_image = self.resize(final_image.clone().movedim(-1, 1), **resize_output_params).movedim(1, -1)
+            if only_mask_area and image is not None and mask is not None:
+                if resize_mask_area_params is not None:
+                    final_image = output_image = self.resize(
+                        output_image.clone().movedim(-1, 1),
+                        **{**resize_mask_area_params, "crop": "disabled", "scale_by": 0, "width": cropped_image_width, "height": cropped_image_height}
+                    ).movedim(1, -1)
 
-                if option_order == "JN_KSamplerSeamlessParams" and seamless_params is not None:
+                final_image = output_image = JN_ImageUncrop().run(destination=input_image, source=output_image, area=mask_area, resize_source=False, mask=cropped_mask)[0]
+
+            seamless_crop_applied = False
+
+            for param in params:
+                if param["__type__"] == "JN_KSamplerResizeOutputParams":
+                    wait_cooldown(kind="execution")
+                    final_image = self.resize(final_image.clone().movedim(-1, 1), **param).movedim(1, -1)
+
+                if param["__type__"] == "JN_KSamplerSeamlessParams" and seamless_params is not None and not seamless_crop_applied:
                     wait_cooldown(kind="execution")
                     final_image = self.seamless_crop(final_image, **seamless_params)
+                    seamless_crop_applied = True
 
-                if option_order == "JN_KSamplerFaceRestoreParams" and face_restore_params is not None:
+                if param["__type__"] == "JN_KSamplerFaceRestoreParams":
                     wait_cooldown(kind="execution")
-                    final_image = self.facerestore(final_image, **face_restore_params)
+                    final_image = self.facerestore(final_image, **param)
 
         if resize_output_params is not None:
-            output_latent["samples"] = self.resize(output_latent["samples"], samples_type="latent", **resize_output_params)
+            for param in resize_output_params:
+                output_latent["samples"] = self.resize(output_latent["samples"], samples_type="latent", **param)
 
-        return (output_latent, output_image, final_image, latent_image, image)
+        return (output_latent, output_image, final_image, latent_image, input_image, input_mask)
 
     def tiled_ksampler(self, width, height, overlay_percent, steps_chunk=0, common_ksampler_params=None, seamless_params=None, **kwargs):
         latent_image = common_ksampler_params["latent"]
@@ -639,17 +723,14 @@ class JN_KSampler:
         return output_latent
 
     def resize(self, samples, width, height, method, crop, upscale_model=None, scale_by=0, samples_type="image", **kwargs):
-        if samples_type == "latent":
-            width = width // 8
-            height = height // 8
-
-        if scale_by > 0:
-            width = round(samples.shape[3] * scale_by)
-            height = round(samples.shape[2] * scale_by)
-        elif width == 0:
-            width = max(1, round(samples.shape[3] * height / samples.shape[2]))
-        elif height == 0:
-            height = max(1, round(samples.shape[2] * width / samples.shape[3]))
+        (width, height) = self.compute_size(
+            original_width=samples.shape[3],
+            original_height=samples.shape[2],
+            width=width,
+            height=height,
+            scale_by=scale_by,
+            samples_type=samples_type
+        )
 
         if width == samples.shape[3] and height == samples.shape[2]:
             return samples
@@ -662,6 +743,21 @@ class JN_KSampler:
                 return samples
 
         return comfy.utils.common_upscale(samples, width, height, method, crop)
+
+    def compute_size(self, original_width, original_height, width, height, scale_by=0, samples_type="image", **kwargs):
+        if samples_type == "latent":
+            width = width // 8
+            height = height // 8
+
+        if scale_by > 0:
+            width = round(original_width * scale_by)
+            height = round(original_height * scale_by)
+        elif width == 0:
+            width = max(1, round(original_width * height / original_height))
+        elif height == 0:
+            height = max(1, round(original_height * width / original_width))
+
+        return (width, height)
 
     def facerestore(self, image, facerestore_model, facedetection, center_direction, strength, **kwargs):
         outputs = JN_FaceRestoreWithModel().restore_face(image=image, facerestore_model=facerestore_model, facedetection=facedetection, strength=strength)
@@ -734,6 +830,7 @@ NODE_CLASS_MAPPINGS = {
     "JN_KSamplerAdvancedParams": JN_KSamplerAdvancedParams,
     "JN_KSamplerResizeInputParams": JN_KSamplerResizeInputParams,
     "JN_KSamplerResizeOutputParams": JN_KSamplerResizeOutputParams,
+    "JN_KSamplerResizeMaskAreaParams": JN_KSamplerResizeMaskAreaParams,
     "JN_KSamplerSeamlessParams": JN_KSamplerSeamlessParams,
     "JN_KSamplerTileParams": JN_KSamplerTileParams,
     "JN_KSamplerFaceRestoreParams": JN_KSamplerFaceRestoreParams,
@@ -745,6 +842,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "JN_KSamplerAdvancedParams": "KSampler Advanced Params",
     "JN_KSamplerResizeInputParams": "KSampler Resize Input Params",
     "JN_KSamplerResizeOutputParams": "KSampler Resize Output Params",
+    "JN_KSamplerResizeMaskAreaParams": "KSampler Resize Mask Area Params",
     "JN_KSamplerSeamlessParams": "KSampler Seamless Params",
     "JN_KSamplerTileParams": "KSampler Tile Params",
     "JN_KSamplerFaceRestoreParams": "KSampler Face Restore Params",
